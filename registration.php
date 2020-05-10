96ebdece0c5889f5254c38463438c6dba98fea7d
Nicolas MEDINA <nicolas@magemontreal.com>
2020-05-10 17:31:53 -0400

    First Commit

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @author MageMontreal
+ * @copyright Copyright © 2020 MageMontreal. All rights reserved.
+ * @package MageMontreal_Core
+ */
+
+use \Magento\Framework\Component\ComponentRegistrar as CR;
+
+CR::register(CR::MODULE, 'MageMontreal_Core', __DIR__);
